8ede80d833fa9756d3df2684897434f670880d51
Divin Irakiza <irakizadivin@gmail.com>
2021-07-07 10:12:10 +0200

    (feat):  postgres DB setup

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 DEBUG = True
@@ -15,12 +14,12 @@ WSGI_APPLICATION = 'app.wsgi.dev.application'
 ASGI_APPLICATION = 'app.asgi.application'
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('DB_NAME'),
-       'USER': os.getenv('DB_USER'),
-       'PASSWORD': os.getenv('DB_PASSWORD'),
-       'HOST': os.getenv('DB_HOST'),
-       'PORT': os.getenv('DB_PORT'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
