106f97ac45c99c917f9c6559f0e4f89df4c4bcb1
Divin Irakiza <irakizadivin@gmail.com>
2021-07-07 09:29:39 +0200

    (feat):  Added DRF configurations

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'corsheaders'
+    'corsheaders',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
